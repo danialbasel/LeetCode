ba5fa8e75b50754c7da3afde3f1c400658467e18
BI Danial Basel Said Abzadough <bi_danial@DSC.gov.ae>
2024-09-10 11:48:15 +0400

    169 ns 84.51%

---
+++ Convert 1D Array Into 2D Array/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+var construct2DArray = function(original, m, n) {
+    if(m*n !== original.length) return [];
+    let twoD = [];
+    for(let i=0;i<m;i++){
+        twoD.push(original.slice(i*n,(i+1)*n ))
+    }
+    return twoD;
+};
